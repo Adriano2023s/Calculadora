2eb25ac3abc3e8897fbbdd1044a77db47c36937f
Adriano Sousa <adrianodesousa1998@gmail.com>
2024-05-25 13:46:50 -0300

    Calculadora pronta

--- Program.cs
+++ Program.cs
@@ -6,7 +6,48 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double nun1;
+            double nun2;
+            int resultado = 0;
+
+            Console.WriteLine("Digite um número \n");
+            nun1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite outro número \n");
+            nun2 = double.Parse(Console.ReadLine());
+
+            Console.Clear();
+
+            while (resultado != 5)
+            {
+                Console.WriteLine("Para somar digite 1:");
+                Console.WriteLine("Para subtrair digite 2:");
+                Console.WriteLine("Para multiplicar digite 3:");
+                Console.WriteLine("Para dividir digite 4:");
+                Console.WriteLine("Para sair digite 5:");
+                resultado = int.Parse(Console.ReadLine());
+
+                if (resultado == 1)
+                {
+                    Console.WriteLine("Soma = {0}", nun1 + nun2);
+                }
+                if (resultado == 2)
+                {
+                    Console.WriteLine("Subtração = {0}", nun1 + nun2);
+                }
+                if(resultado == 3)
+                {
+                    Console.WriteLine("Multiplicação = {0}", nun1 + nun2);
+                }
+                if (resultado == 4)
+                {
+                    Console.WriteLine("Divizão = {0}", nun1 + nun2);
+                }
+                if (resultado == 5)
+                {
+                    Console.WriteLine("Programa enserrado");
+                }
+            }
         }
     }
 }
--- obj/Debug/netcoreapp3.1/Calculadora.AssemblyInfo.cs
+++ obj/Debug/netcoreapp3.1/Calculadora.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5046c998c12fd29489e449f9c55c7fc090a2f7e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c70883625927670b78ad870654f620fdb3273ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
